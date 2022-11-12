3d3c3711b13ff177fd058f48a414b12394f48ea6
srini-equitasit <srini@equitasitinc.com>
2022-11-12 21:23:59 +0530

    updated the ip info

--- src/main/java/com/equitasit/ms/controller/UserController.java
+++ src/main/java/com/equitasit/ms/controller/UserController.java
@@ -1,18 +1,40 @@
 package com.equitasit.ms.controller;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import java.net.InetAddress;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("user")
+@Slf4j
 public class UserController {
 
 	@GetMapping("hello/{user}")
 	public String hello(@PathVariable("user") String user) {
 
-		return "hello " + user;
+		log.info("start request ");
+		String message = "";
+		try {
+			InetAddress ip = InetAddress.getLocalHost();
+			message += " From Host : " + ip;
+		} catch (UnknownHostException e) {
+			log.error("error while getting the ip", e);
+		}
+
+		message = "hello " + user + " serving " + message;
+
+		log.info("message = " + message);
+
+		log.info("end request ");
+
+		return message;
 	}
 
 }
